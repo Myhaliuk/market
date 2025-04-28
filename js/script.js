8a69d16637636192fa107612b00af014d9e6b837
Myhaliuk <24a_myav@liceum.ztu.ua>
2025-04-28 09:33:53 +0300

    виправлено помилки з js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
-console.log('перевірка підключеного файлу script.js')
-let itemsDiv = document.getElementById("items")
-if (itemsDiv) {
-    console.log(itemsDiv)
-
- for(let i=0; i < 100; i++) {
-itemsDiv.innerHTML+='<div class = "item"></div>'
- }
-
-  
-} else {
-    console.log('Блок товарів не знайдено')
-}
-
-
 
 let itemsArray = ['мотокоса 43',
     'газонокосилка',
     'акумолятор 54',
     'машина часу v.2',]
 
+let itemsDiv = document.getElementById("items")
 
+if (itemsDiv) {
 
+ itemsArray.forEach((item) => {
 
-for (let i = 0; i < temsArray.length; i++) {
-    console.log(i + '-й елемент:', itemsArray[i]) 
-} 
+itemsDiv.innerHTML +=`<div class = "item">${item}</div>`
+
+})
+
+} else {
+    console.log('Блок товарів не знайдено')
+}
